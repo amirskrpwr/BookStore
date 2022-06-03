af8dff45e1687dda2de5de3238d9a731464a8e06
Amirhossein <amhskrpwr@gmail.com>
2022-06-03 20:10:40 +0430

    little changes.

--- store/views.py
+++ store/views.py
@@ -203,7 +203,6 @@ def current_user(request):
 #     return Response({"data":"Successfully added."}, status=status.HTTP_201_CREATED)
 
 class CustomerModification(APIView):
-    # permission_classes = [permission_classes]
     parser_classes = [MultiPartParser, FormParser]
 
     def post(self, request, format=None):
@@ -221,7 +220,6 @@ class CustomerModification(APIView):
         )
 
         if serializer.is_valid():
-            # serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
